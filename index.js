becb047ce88929ec39e87428536580a819efc3bc
SteVen Batten <stbatt@microsoft.com>
2018-10-30 17:21:58 -0700

    format the code

--- index.js
+++ index.js
@@ -29,20 +29,20 @@ app.use(session({
 }))
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
-    res.locals.session = req.session;
-    next();
+app.use(function (req, res, next) {
+  res.locals.session = req.session;
+  next();
 });
 
 app.use('/', index);
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
